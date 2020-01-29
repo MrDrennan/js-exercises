417a905a463e85d8cd138915d8ebcf8964c5e99c
Chad Drennan <cdrennan@mail.greenriver.edu>
2020-01-29 02:29:35 -0500

    wrote html, test method, and CSS

--- js4/array-object.js
+++ js4/array-object.js
@@ -1,3 +1,5 @@
+testCreateObject();
+
 function createObjectWithTypeProperties(array) {
     newObject = {};
 
@@ -13,4 +15,30 @@ function createObjectWithTypeProperties(array) {
         }
     }
     return newObject;
+}
+
+function testCreateObject() {
+    let inputs = [[-1, 5, "cat", false, 10.2, true, "dog"]];
+    inputs.push(["Test", 1, false]);
+    inputs.push([[1, 2, 3], "wolf", 12, {x: 3, y: 4}, "tiger", 3.14]);
+    inputs.push([null, "bird", "fish", 0, -1.5]);
+    inputs.push([]);
+
+    for (let currInput of inputs) {
+        let output = createObjectWithTypeProperties(currInput);
+
+        displayResult(currInput, output);
+    }
+}
+
+function displayResult(input, output) {
+    output = JSON.stringify(output);
+    input = JSON.stringify(input);
+
+    let newRow = document.createElement('tr');
+    newRow.innerHTML = "<td>" + input + "</td><td>" + output + "</td>";
+
+    let tBody = document.getElementsByTagName("tbody")[0];
+
+    tBody.appendChild(newRow);
 }
